a8fafa1c72c22148fbcc4492720ca9adad8ace14
NextGoalIs <wlswls8954@gmail.com>
2022-07-08 17:06:14 +0900

    arrow up func

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const menus = document.querySelectorAll(".navbar__menu li");
 const sections = Array.from(document.querySelectorAll("section"));
+const arrowUp = document.querySelector(".arrow-up");
 
+//메뉴 클릭시 화면 이동
 const NAVBAR__HEIGHT = 99;
 
 function menuClickHandler(e) {
@@ -16,4 +18,21 @@ function menuClickHandler(e) {
 
 menus.forEach((menu) => {
   menu.addEventListener("click", menuClickHandler);
+});
+
+//화면 일정 이상 내려갈 시 Up 버튼 생성
+document.addEventListener("scroll", () => {
+  if (scrollY >= NAVBAR__HEIGHT) {
+    arrowUp.classList.add("active");
+  } else {
+    arrowUp.classList.remove("active");
+  }
+});
+
+//Up 버튼 클릭 시 맨 위로
+arrowUp.addEventListener("click", () => {
+  scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
